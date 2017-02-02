67638186b601cc79a55c3fe43a4dfeb9036f5b5a
nikhilbytes <nikhil.pandit1596@gmail.com>
2017-02-02 16:35:35 +0530

    Create MainTest.java

---
+++ src/com/sqltojson/MainTest.java
@@ -0,0 +1,18 @@
+
+import com.sqltojson.SqltableToJSON
+
+Class MainTest
+{
+public static void main(String args[])
+{
+public String getJSONString(int idRoot){
+return SqltableToJSON.(idRoot);
+}
+ 
+public Object getJsonEquivJavaObject(int idRoot){
+return SqltableToJSON.getJsonObject(idRoot);
+} 
+System.out.println(getJSONString(idRoot));
+
+}
+}
